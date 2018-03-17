e3c4d689f0218c9cbfb62bbf41abffcb6cdb1e4b
= <=>
2018-03-17 05:23:43 -0500

    config the model

--- models/user.js
+++ models/user.js
@@ -8,28 +8,37 @@ const SALT_WORK_FACTOR = 10;
 }
 const UserSchema = new mongoose.Schema({
     username: {required: true, type: String, unique: true, set: toLower},
-    password: {required: true, type: String},
+    password: {type: String},
     email: {required: true, type: String, unique: true, set: toLower},
     first_name: {required: true, type: String},
     last_name: {required: true, type: String}
 });
+//generating the hash
+// UserSchema.methods.generateHash = (password) => {
+//     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+// }
+// //checking if password is valid
+// UserSchema.methods.validPassword = (password) => {
+//     return bcrypt.compareSync(password, this.local.password);
+// }
 
 //for encryption
-UserSchema.pre('save', next => {
-    let user = this;
-    if (!user.isModified('password'))
-    return next();
+// UserSchema.pre('save', next => {
+//     let user = this;
+//     if (!user.isModified('password'))
+//     return next();
 
-    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-        if (err) return next(err);
-        bcrypt.hash(user.password, salt, (err, hash) => {
-            if (err) return next(err);
-            user.password = hash;
-            next();
-        });
-    });
-})
+//     bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+//         if (err) return next(err);
+//         bcrypt.hash(user.password, salt, (err, hash) => {
+//             if (err) return next(err);
+//             user.password = hash;
+//             next();
+//         });
+//     });
+// })
 
 UserSchema.plugin(passportLocalMongoose);
 
-export default mongoose.model('User', UserSchema);
+// export default mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
