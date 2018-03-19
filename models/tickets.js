4fe1f7d204f91e27f8d8ce01c8a0d10a488d6739
= <=>
2018-03-19 05:19:02 -0500

    use ES6

--- models/tickets.js
+++ models/tickets.js
@@ -1,13 +1,21 @@
-const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
+
+import mongoose from 'mongoose';
+import passportLocalMongoose from 'passport-local-mongoose';
+let responseComplain = require('../models/responseComplain');
+let User = require('../models/user');
+let Ticket = require('../models/tickets');
 
 const TicketSchema = new mongoose.Schema({
-    username: {required: true, type: String},
+    user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    //admin: {type: new mongoose.Schema.Types.ObjectId, ref: 'Admin'},
     subject: {required: true, type: String},
     date_time: {required: true, type: Date},
-    status: {required: true, type: String}
+    status: {required: true, type: String, default: 'pending'},
+},
+{
+    timestamps: true
 });
 
 TicketSchema.plugin(passportLocalMongoose);
 
-export default mongoose.model('Ticket', TicketSchema);
+module.exports = mongoose.model('Ticket', TicketSchema);
